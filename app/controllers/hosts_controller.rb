744c9a18913117c491ef9b27fa8c78ec45acf2fc
jonopens <jonopenshaw@gmail.com>
2017-10-02 10:38:46 -0400

    added basic crud actions for host class and ran migrations

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,29 +1,49 @@
 class HostsController < ApplicationController
 
   def index
+    @hosts = Host.all
   end
 
   def show
+    @host = Host.find(params[:id])
   end
 
   def new
+    @host = Host.new
   end
 
   def create
+    @host = Host.new(host_params)
+    if @host.save
+      redirect_to host_path(@host)
+    else
+      render :new
+    end
   end
 
   def edit
+    @host = Host.find(params[:id])
   end
 
   def update
+    @host = Host.find(param[:id])
+    if @host.update(host_params)
+      redirect_to host_path(@host)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @host = Host.find(params[:id])
+    @host.destroy
+    redirect_to hosts_path
   end
 
   private
 
   def host_params
+    params.require(:host).permit(:username)
   end
 
 end
