0928c844fd1d8f5b89a28de216d791bb235fd14c
ian-ramos <ramos.ianc@gmail.com>
2017-10-02 10:39:40 -0400

    added form in new template of entertainers

--- app/controllers/entertainers_controller.rb
+++ app/controllers/entertainers_controller.rb
@@ -1,29 +1,49 @@
 class EntertainersController < ApplicationController
 
   def index
+    @entertainers = Entertainer.all
   end
 
   def show
+    @entertainer = Entertainer.find(params[:id])
   end
 
   def new
+    @entertainer = Entertainer.new
   end
 
   def create
+    @entertainer = Entertainer.new(entertainer_params)
+    if @entertainer.save
+      redirect_to entertainer_path(@entertainer)
+    else
+      render :new
+    end
   end
 
   def edit
+    @entertainer = Entertainer.find(params[:id])
   end
 
   def update
+    @entertainer = Entertainer.find(params[:id])
+    if @entertainer.update(entertainer_params)
+      redirect_to entertainers_path(@entertainer)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @entertainer = Entertainer.find(params[:id])
+    @entertainer.destroy
+    redirect_to entertainers_path
   end
 
   private
 
   def entertainer_params
+    params.require(:entertainer).permit(:username, :job_type)
   end
 
 end
