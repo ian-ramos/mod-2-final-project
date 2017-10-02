582a525a679ee48fc2eea0012714a8549aa7d840
ian-ramos <ramos.ianc@gmail.com>
2017-10-02 11:51:56 -0400

    added login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if @host = Host.find_by(username: params[:username])
+      session[:host_id] = @host.id
+      redirect_to host_path(@host)
+    else
+      @entertainer = Entertainer.find_by(username: params[:username])
+      session[:entertainer_id] = @entertainer.id
+      redirect_to entertainer_path(@entertainer)
+    end
   end
 
   def destroy
+    session.delete :host_id
+    session.delete :entertainer_id
+    redirect_to login_path
   end
 
 end
--- app/models/entertainer.rb
+++ app/models/entertainer.rb
@@ -1,5 +1,8 @@
 class Entertainer < ApplicationRecord
 
+  has_many :events
+  has_many :hosts, through: :events
+
   @@job_types = ["Musician", "Clown", "Magician", "Belly Dancer", "Animal Tamer"]
 
   def self.job_types
