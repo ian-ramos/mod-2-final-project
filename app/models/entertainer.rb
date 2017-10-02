d00232a6788087e0dbd7dd6234beca676874110a
ian-ramos <ramos.ianc@gmail.com>
2017-10-02 09:48:01 -0400

    added job types array to entertainer

--- app/models/entertainer.rb
+++ app/models/entertainer.rb
@@ -1,2 +1,9 @@
 class Entertainer < ApplicationRecord
+
+  @@job_types = ["Musician", "Clown", "Magician", "Belly Dancer", "Animal Tamer"]
+
+  def self.job_types
+    @@jobs_types
+  end
+
 end
--- db/migrate/20171002132915_create_events.rb
+++ db/migrate/20171002132915_create_events.rb
@@ -3,6 +3,8 @@ class CreateEvents < ActiveRecord::Migration[5.1]
     create_table :events do |t|
       t.string :name
       t.text :description
+      t.integer :host_id
+      t.integer :entertainer_id
       t.datetime :date
 
       t.timestamps
