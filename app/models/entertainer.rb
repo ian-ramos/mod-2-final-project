828618af557e01f7eb2e569064782945c364e773
ian-ramos <ramos.ianc@gmail.com>
2017-10-02 13:32:28 -0400

    fixed associations

--- app/models/entertainer.rb
+++ app/models/entertainer.rb
@@ -2,11 +2,17 @@ class Entertainer < ApplicationRecord
 
   has_many :events_entertainers
   has_many :events, through: :events_entertainers
+  validates :username, presence: true, uniqueness: true, length: {in: 5..30}
 
   @@job_types = ["Musician", "Clown", "Magician", "Belly Dancer", "Animal Tamer"]
 
   def self.job_types
     @@job_types
+  end
+
+  def events
+    @events = Entertainer.events_entertainers.event_ids
+    
   end
 
 end
