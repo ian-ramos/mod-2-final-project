03f7ec30626a6afca41a6f8167d94d315f710379
ryansperzel <ryansperzel@gmail.com>
2017-10-02 10:45:16 -0400

    Event controller actions and model associations built

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true, length: {maximum: 500}, length: {minimum: 10}, uniqueness: {scope: :host_id, message: "You already have an event that day!"}
+  validates :date, presence: true
+
+
+  belongs_to :host
+  belongs_to :entertainer
+
 end
