f4bd3af7c0f8e20097b337d101207ee8e66627d2
jonopens <jonopenshaw@gmail.com>
2017-10-02 11:55:26 -0400

    added more seeds and fixed hosts#show view

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 Entertainer.create(username: "jonopens", job_type: "Magician")
 Entertainer.create(username: "ianramos", job_type: "Animal Tamer")
 Entertainer.create(username: "ryansperzel", job_type: "Clown")
+Host.create(username: "spiderman")
+Host.create(username: "green-goblin")
+Host.create(username: "venom")
+Event.create(name: "Old guys playing bocce", description: "it's old guys playing.", host_id: 1, entertainer_id: 3, date: Time.now)
