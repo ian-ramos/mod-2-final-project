13604dda721712f0538e0ae157570865f100c822
ian-ramos <ramos.ianc@gmail.com>
2017-10-03 10:38:18 -0400

    added duration for event & default for closed status

--- db/migrate/20171002182731_add_closed_status_to_events.rb
+++ db/migrate/20171002182731_add_closed_status_to_events.rb
@@ -1,5 +1,5 @@
 class AddClosedStatusToEvents < ActiveRecord::Migration[5.1]
   def change
-    add_column :events, :closed_status, :boolean
+    add_column :events, :closed_status, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003131914) do
+ActiveRecord::Schema.define(version: 20171003142405) do
 
   create_table "entertainers", force: :cascade do |t|
     t.string "username"
@@ -30,7 +30,8 @@ ActiveRecord::Schema.define(version: 20171003131914) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "closed_status"
+    t.boolean "closed_status", default: false
+    t.string "duration"
   end
 
   create_table "events_entertainers", force: :cascade do |t|
