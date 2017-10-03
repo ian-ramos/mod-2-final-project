6d5dba6cbfb4a0e14c88db182a98e4d6f8e9def2
ryansperzel <ryansperzel@gmail.com>
2017-10-03 11:00:25 -0400

    Added authentication to hosts and logout to header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :require_login
 
   def current_user
     if session[:host_id]
@@ -15,5 +15,12 @@ class ApplicationController < ActionController::Base
   end
 
   def welcome
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to login_path
+    end
   end
 end
